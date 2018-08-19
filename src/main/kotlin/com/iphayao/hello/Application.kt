a93b750205f7600c27b21f1295ce5c3cf647766d
iphayao <phayaob@gmail.com>
2018-08-19 16:13:54 +0700

    Add implement simple endpoint

---
+++ src/main/kotlin/com/iphayao/hello/Application.kt
@@ -0,0 +1,11 @@
+package com.iphayao.hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinApplication>(*args)
+}
---
+++ src/main/kotlin/com/iphayao/hello/GreetingController.kt
@@ -0,0 +1,16 @@
+package com.iphayao.hello
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.reflect.jvm.internal.impl.load.java.descriptors.StringDefaultValue
+
+@RestController
+class GreetingController {
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+}
